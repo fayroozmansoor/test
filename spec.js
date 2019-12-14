4d4c12a0c6fbcfe7c45c23fae71f16e746fe43d6
fayroozmansoor <41577037+fayroozmansoor@users.noreply.github.com>
2019-12-14 21:07:49 +0300

    Update spec.js

--- spec.js
+++ spec.js
@@ -10,8 +10,7 @@ describe('angularjs homepage todo list', function() {
     console.log(title);
     element(by.id('global-search-input')).sendKeys('bag')
     element(by.id('global-search-submit')).click()
-//element(by.className("product-title-link")).click()
-//element(by.css("img[src*='images/logo.png']"));
+
 element(by.xpath('//img[@alt="Great Value Double Zipper Sandwich Bags, 300 Count"]')).click()
 element(by.xpath('//div[@class="display-inline-block l-margin-right QuantityPicker-select font-semibold field field--select field--secondary"]')).click()
 element(by.xpath('//select[@placeholder="Qty:"]'))
