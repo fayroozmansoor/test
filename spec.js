02f16f8754be039fee44c588f16e34f1c6142701
fayroozmansoor <41577037+fayroozmansoor@users.noreply.github.com>
2019-12-22 22:27:21 +0300

    Update spec.js

--- spec.js
+++ spec.js
@@ -8,17 +8,16 @@ describe('angularjs homepage todo list', function() {
 
     var title = browser.getTitle();
     console.log(title);
-    element(by.id('global-search-input')).sendKeys('bag')
-    element(by.id('global-search-submit')).click()
+    element(by.id('global-search-input')).sendKeys('bag');
+    element(by.id('global-search-submit')).click();
 
-element(by.xpath('//img[@alt="Great Value Double Zipper Sandwich Bags, 300 Count"]')).click()
-element(by.xpath('//div[@class="display-inline-block l-margin-right QuantityPicker-select font-semibold field field--select field--secondary"]')).click()
-element(by.xpath('//select[@placeholder="Qty:"]'))
+element(by.xpath('//img[@alt="Great Value Double Zipper Sandwich Bags, 300 Count"]')).click();
+element(by.xpath('//div[@class="display-inline-block l-margin-right QuantityPicker-select font-semibold field field--select field--secondary"]')).click();
+element(by.xpath('//select[@placeholder="Qty:"]'));
 
-element(by.xpath('//select[@option="3"]/option[2]')).click()
-
-element(by.xpath('//button[@class=class="button spin-button prod-ProductCTA--primary button--primary"]')).click
-
-    browser.pause();  });
-
-  });
+element(by.xpath('//select[@aria-label="Quantity"]/option[text()="3"]')).click();
+//element(by.xpath('//');
+element(by.xpath('//button[@class="button spin-button prod-ProductCTA--primary button--primary"]')).click();
+browser.pause();
+   });
+});
